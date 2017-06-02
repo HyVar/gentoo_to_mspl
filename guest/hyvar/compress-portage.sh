b523ff69164f0886f2f7ec50f026317717b2c446
gzoumix <mlienhar@petunia.dipinfo.di.unito.it>
2017-06-02 02:57:57 +0200

    In progress: manage deprecated and refactor translator

--- guest/hyvar/compress-portage.sh
+++ guest/hyvar/compress-portage.sh
@@ -5,4 +5,22 @@ cd /home/osboxes/hyvar
 
 [ -d gen ] || mkdir gen
 
-tar cvfj gen/portage.tar.bz2 /usr/portage/metadata/md5-cache
+# get the deprecated files
+mkdir -p /tmp/deprecated
+for path in $(find /var/db/pkg -name "*.ebuild"); do
+  path=${path#/var/db/pkg}
+  CATEGORY=${path%%*/}
+  FILE=${path#*/}
+  TEST=${FILE##*-}
+  PACKAGE=${FILE%*-}
+  if [ ${TEST: -1} = "r" ]; then
+  	PACKAGE=${PACKAGE%*-}
+  fi
+  PACKAGE=/tmp/deprecated/${PACKAGE}
+  mkdir -p ${PACKAGE}
+  cp ${path} ${PACKAGE} 
+done
+egencache --update --repo
+
+
+tar cvfj gen/portage.tar.bz2 /usr/portage/metadata/md5-cache /tmp/deprecated/metadata/md5-cache
--- setup-guest.sh
+++ setup-guest.sh
@@ -11,4 +11,6 @@ else
 
  sshpass -p $PSWD_USER ssh -o PubkeyAuthentication=no -p ${PORT} ${HOST} "echo ${PSWD} | sudo -S -- sh -c 'echo ACCEPT_KEYWORDS=\\\"~amd64\\\" >> /etc/portage/make.conf'"
  sshpass -p$PSWD_USER scp -o PubkeyAuthentication=no -P ${PORT}  -r guest/*  ${HOST}:
+ sshpass -p $PSWD_USER ssh -o PubkeyAuthentication=no -p ${PORT} ${HOST} "echo ${PSWD} | sudo -S -- mv ~hyvar/repos.conf /etc/portage/"
+
 fi
