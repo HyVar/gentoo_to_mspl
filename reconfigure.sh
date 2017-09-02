97fe124c03917ac425fa2aaaec78c51fb8c3df40
gzoumix <mlienhar@petunia.dipinfo.di.unito.it>
2017-09-03 01:38:26 +0200

    bit of cleaning

--- reconfigure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REQUEST_FILE=$1
-ENV=$2
-
-# e.g., ENV=amd64
-
-python host/scripts/portage2hyvarrec/reconfigure.py -v --environment $ENV host/portage/json/hyvarrec $REQUEST_FILE host/configuration/json/configuration.json host/configuration/json/new_configuration.json host/configuration/package.use host/configuration/update.sh
-
