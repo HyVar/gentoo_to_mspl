a8b2ee1bb1238d93bbfce550ff4de4a9a43143ab
gzoumix <michael.lienhardt@laposte.net>
2017-10-31 03:18:00 +0100

    added new code for statistics in vamos paper. not tested yet

--- host/statistics/db.py
+++ host/statistics/db.py
@@ -1,12 +1,15 @@
 
+#######################################
+# This file offers a simple interface that loads the hyportage data and give direct pointers to it
+#######################################
+
 
 import os
 
-import hyportage
+import core_data
+
 import hyportage_ids
-import hyportage_data
 import hyportage_pattern
-import hyportage_configuration
 
 import utils
 
@@ -20,29 +23,49 @@ __email__ = "michael.lienhardt@laposte.net"
 __status__ = "Prototype"
 
 
-db_hyportage_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data/hyportage/hyportage.enc"))
+# the path and encoding of the hyportage data
+db_hyportage_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data/hyportage/hyportage.pickle"))
 db_hyportage_save_modality = "pickle"
 
 
+# the path and encoding of the configuration data
+db_configuration_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data/portage/config.pickle"))
+db_configuration_save_modality = "pickle"
+
+
 hyportage_db_loaded = False
+# hyportage data
 pattern_repository = hyportage_pattern.pattern_repository_create()
 flat_pattern_repository = {}
 id_repository = hyportage_ids.id_repository_create()
-keywords = set()
 mspl = {}
 spl_groups = {}
-core_configuration = hyportage_configuration.core_configuration_create()
-installed_spls = {}
+# configuraton data
+config = core_data.Config()
+mspl_config = core_data.MSPLConfig()
+keyword_list = None
+installed_packages = None
+world = None
+
 
 def load_hyportage():
 	utils.phase_start("Loading the hyportage database.")
-	global hyportage_db_loaded, pattern_repository, flat_pattern_repository, id_repository, keywords, mspl, spl_groups, core_configuration, installed_spls
+	global hyportage_db_loaded
+	global pattern_repository, flat_pattern_repository, id_repository, mspl, spl_groups
+	global config, mspl_config, keyword_list, installed_packages, world
 	if not hyportage_db_loaded:
-		pattern_repository, id_repository, mspl, spl_groups, core_configuration, installed_spls = hyportage.load_hyportage(
-			db_hyportage_path, db_hyportage_save_modality)
-		flat_pattern_repository = pattern_repository[1]
+		pattern_repository, id_repository, mspl, spl_groups = utils.load_data_file(db_hyportage_path, db_hyportage_save_modality)
+		flat_pattern_repository = pattern_repository[1].copy()
 		for local_map in pattern_repository[0].values():
 			flat_pattern_repository.update(local_map)
-		keywords = hyportage_data.keywords_get_core_set(hyportage_ids.id_repository_get_keywords(id_repository)[0])
+		config = utils.load_data_file(db_configuration_path, db_configuration_save_modality)
+		mspl_config = config.mspl_config
+		keyword_list = config.keyword_list
+		installed_packages = config.installed_packages
+		world = config.world
 		hyportage_db_loaded = True
 	utils.phase_end("Loading Completed")
+
+
+def filter_function_simple(x):
+	return True
---
+++ host/statistics/graph.py
@@ -0,0 +1,64 @@
+
+#######################################
+# This file computes several graphs from the hyportage data
+#######################################
+
+import hyportage_data
+import hyportage_pattern
+
+import db
+
+from tulip import tlp
+
+
+def get_graph_mspl(filter_function=db.filter_function_simple):
+	graph = tlp.newGraph()
+	map = {}
+	for spl in db.mspl.itervalues():
+		if filter_function(spl):
+			node = graph.addNode()
+			map[spl] = node
+			graph['viewLabel'][node] = spl.name
+
+	spls = set(map.keys())
+
+	for spl, node in map.iteritems():
+		dependency_list = {
+			dep
+			for pattern in spl.dependencies.iterkeys()
+			for dep in hyportage_pattern.pattern_repository_get(db.pattern_repository, pattern).get_spls(db.mspl, db.spl_groups)
+		}
+		for dep in dependency_list & spls:
+			if spl != dep:
+				graph.addEdge(map[spl], map[dep])
+
+	return graph
+
+
+def get_graph_spl_groups(filter_function=db.filter_function_simple):
+	graph = tlp.newGraph()
+	map = {}
+	for group in db.spl_groups.itervalues():
+		if filter_function(group):
+			node = graph.addNode()
+			map[group] = node
+			graph['viewLabel'][node] = group.name
+
+	groups = set(map.keys())
+
+	for group, node in map.iteritems():
+		dependency_list = {
+			dep
+			for spl in group.references
+			for pattern in spl.dependencies.iterkeys()
+			for dep in hyportage_pattern.pattern_repository_get(db.pattern_repository, pattern).get_spls(db.mspl, db.spl_groups)
+		}
+		group_dependency_list = {db.spl_groups[hyportage_data.spl_get_group_name(spl)] for spl in dependency_list}
+		for dep in group_dependency_list & groups:
+			if group != dep:
+				graph.addEdge(map[group], map[dep])
+
+	return graph
+
+
+
